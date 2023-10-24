25fb7f95ded89b7d0a9c22ba33e34f4fad036497
careb36 <carolina.pereira@estudiantes.utec.edu.uy>
2023-10-23 23:03:59 -0300

    actualizo v2

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# SQL_com_47370
-## *Contenidos centrales del curso:* 
+#SQL_com_47370
+##*Contenidos centrales del curso:*
 - Entender los conceptos básicos de las bases de datos relacionales. 
-- Crear una base de datos relacional desde cero. 
-- Insertar información en una base de datos relacional. 
+- Crear una base de datos relacional desde cero.
+- Insertar información en una base de datos relacional.
 - Generar consultas SQL avanzadas para analizar datos.
-
-
--- src/montevideo_noticias.sql
+++ src/montevideo_noticias.sql
@@ -1,57 +1,135 @@
 -- Crear la base de datos Montevideo Noticias
 CREATE DATABASE montevideo_noticias;
 USE montevideo_noticias;
--- Crear la tabla de artículos
-CREATE TABLE `articulos` (
-`id` integer PRIMARY KEY,
-`titulo` char,
-`cuerpo` text,
-`autor_id` integer,
-`categoria_id` integer,
-`creado_en` timestamp,
-`actualizado_en` timestamp,
-`imagen` varchar(255)
+
+-- Crear la tabla de usuarios
+CREATE TABLE `usuarios` (
+  `id` integer PRIMARY KEY,
+  `nombre` char,
+  `apellido` char,
+  `email` char UNIQUE NOT NULL,
+  `fecha_nacimiento` date,
+  `pais` char,
+  `localidad` char,
+  `foto_perfil` varchar(255),
+  `creado_en` timestamp NOT NULL,
+  `actualizado_en` timestamp NOT NULL
+);
+
+-- Crear la tabla de categorías
+CREATE TABLE `categorias` (
+  `id` integer PRIMARY KEY,
+  `nombre` char UNIQUE NOT NULL,
+  `descripcion` text,
+  `imagen` varchar(255),
+  `creado_en` timestamp NOT NULL,
+  `actualizado_en` timestamp NOT NULL
 );
 
 -- Crear la tabla de autores
 CREATE TABLE `autores` (
-`id` integer PRIMARY KEY,
-`nombre` char,
-`email` char,
-`creado_en` timestamp,
-`actualizado_en` timestamp
+  `id` integer PRIMARY KEY,
+  `nombre` char,
+  `apellido` char,
+  `biografia` text,
+  `redes_sociales` text,
+  `email` char UNIQUE NOT NULL,
+  `creado_en` timestamp NOT NULL,
+  `actualizado_en` timestamp NOT NULL
 );
--- Crear la tabla de categorías
-CREATE TABLE `categorias` (
-`id` integer PRIMARY KEY,
-`nombre` char,
-`creado_en` timestamp,
-`actualizado_en` timestamp
+
+-- Crear la tabla de artículos
+CREATE TABLE `articulos` (
+  `id` integer PRIMARY KEY,
+  `titulo` char,
+  `cuerpo` text,
+  `autor_id` integer,
+  `categoria_id` integer,
+  `resumen` text,
+  `fecha_publicacion` timestamp,
+  `estado_publicacion` char,
+  `imagen` varchar(255),
+  `creado_en` timestamp NOT NULL,
+  `actualizado_en` timestamp NOT NULL,
+  FOREIGN KEY (`autor_id`) REFERENCES `autores` (`id`),
+  FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`)
 );
 
 -- Crear la tabla de comentarios
 CREATE TABLE `comentarios` (
-`id` integer PRIMARY KEY,
-`articulo_id` integer,
-`autor_id` integer,
-`cuerpo` text,
-`creado_en` timestamp,
-`actualizado_en` timestamp
-);
--- Crear la tabla de votos
-CREATE TABLE `votos` (
-`id` integer PRIMARY KEY,
-`articulo_id` integer,
-`autor_id` integer,
-`voto` integer,
-`creado_en` timestamp,
-`actualizado_en` timestamp
+  `id` integer PRIMARY KEY,
+  `articulo_id` integer,
+  `autor_id` integer,
+  `cuerpo` text,
+  `fecha_publicacion` timestamp,
+  `estado_publicacion` char,
+  `creado_en` timestamp NOT NULL,
+  `actualizado_en` timestamp NOT NULL,
+  FOREIGN KEY (`articulo_id`) REFERENCES `articulos` (`id`),
+  FOREIGN KEY (`autor_id`) REFERENCES `usuarios` (`id`)
 );
 
--- Agregar restricciones de clave foránea
-ALTER TABLE `articulos` ADD FOREIGN KEY (`autor_id`) REFERENCES `autores` (`id`);
-ALTER TABLE `articulos` ADD FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`);
-ALTER TABLE `comentarios` ADD FOREIGN KEY (`articulo_id`) REFERENCES `articulos` (`id`);
-ALTER TABLE `comentarios` ADD FOREIGN KEY (`autor_id`) REFERENCES `autores` (`id`);
-ALTER TABLE `votos` ADD FOREIGN KEY (`articulo_id`) REFERENCES `articulos` (`id`);
-ALTER TABLE `votos` ADD FOREIGN KEY (`autor_id`) REFERENCES `autores` (`id`);
+-- Crear la tabla de votos
+CREATE TABLE `votos` (
+  `id` integer PRIMARY KEY,
+  `articulo_id` integer,
+  `autor_id` integer,
+  `voto` tinyint UNSIGNED,
+  `fecha_voto` timestamp,
+  `creado_en` timestamp NOT NULL,
+  `actualizado_en` timestamp NOT NULL,
+  FOREIGN KEY (`articulo_id`) REFERENCES `articulos` (`id`),
+  FOREIGN KEY (`autor_id`) REFERENCES `usuarios` (`id`)
+);
+
+-- Crear la tabla de etiquetas
+CREATE TABLE `etiquetas` (
+  `id` integer PRIMARY KEY,
+  `nombre` char UNIQUE NOT NULL,
+  `creado_en` timestamp NOT NULL,
+  `actualizado_en` timestamp NOT NULL
+);
+
+-- Crear la tabla de artículos_etiquetas para relacionar etiquetas con artículos
+CREATE TABLE `articulos_etiquetas` (
+  `articulo_id` integer,
+  `etiqueta_id` integer,
+  PRIMARY KEY (`articulo_id`, `etiqueta_id`),
+  FOREIGN KEY (`articulo_id`) REFERENCES `articulos` (`id`),
+  FOREIGN KEY (`etiqueta_id`) REFERENCES `etiquetas` (`id`)
+);
+
+-- Crear la tabla de suscriptores
+CREATE TABLE `suscriptores` (
+  `usuario_id` integer,
+  `categoria_id` integer,
+  PRIMARY KEY (`usuario_id`, `categoria_id`),
+  FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`),
+  FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`)
+);
+
+-- Crear la tabla de roles de usuario
+CREATE TABLE `roles_usuario` (
+  `id` integer PRIMARY KEY,
+  `nombre` char UNIQUE NOT NULL
+);
+
+-- Crear la tabla de permisos
+CREATE TABLE `permisos` (
+  `id` integer PRIMARY KEY,
+  `nombre` char UNIQUE NOT NULL
+);
+
+-- Crear la tabla de roles_permisos para relacionar roles y permisos
+CREATE TABLE `roles_permisos` (
+  `rol_id` integer,
+  `permiso_id` integer,
+  PRIMARY KEY (`rol_id`, `permiso_id`),
+  FOREIGN KEY (`rol_id`) REFERENCES `roles_usuario` (`id`),
+  FOREIGN KEY (`permiso_id`) REFERENCES `permisos` (`id`)
+);
+
+-- Crear índices para mejorar el rendimiento de las consultas
+CREATE INDEX idx_email_usuarios ON usuarios (email);
+CREATE INDEX idx_email_autores ON autores (email);
+
