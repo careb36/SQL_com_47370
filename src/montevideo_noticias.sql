2cc1080567432ba32a317bef2985f3b66cf15aee
careb36 <carolina.pereira@estudiantes.utec.edu.uy>
2023-11-11 23:36:06 -0300

    DCL

--- src/montevideo_noticias.sql
+++ src/montevideo_noticias.sql
@@ -415,7 +415,7 @@ END;
 //
 DELIMITER ;
 
--- Triggers for votes
+-- Triggers
 
 -- Trigger BEFORE DELETE para los votos
 DELIMITER //
@@ -438,6 +438,20 @@ BEGIN
 END;
 //
 DELIMITER ;
+
+-- Crea un nuevo usuario llamado 'usu_lectura' con acceso de solo lectura
+CREATE USER 'usu_lectura'@'localhost' IDENTIFIED BY 'readonly12345';
+
+-- Otorgar permiso SELECT (lectura) a 'usu_lectura' en todas las tablas en el esquema montevideo_noticias
+GRANT SELECT ON montevideo_noticias.* TO 'usu_lectura'@'localhost';
+
+
+-- Crea un nuevo usuario llamado 'usu_modifica' con permisos de lectura, inserción y actualización
+CREATE USER 'usu_modifica'@'localhost' IDENTIFIED BY 'modify12345';
+
+-- Otorgar permisos SELECT (lectura), INSERT y UPDATE a 'usu_modifica' en todas las tablas en el esquema montevideo_noticias
+GRANT SELECT, INSERT, UPDATE ON montevideo_noticias.* TO 'usu_modifica'@'localhost';
+
 
 
 
